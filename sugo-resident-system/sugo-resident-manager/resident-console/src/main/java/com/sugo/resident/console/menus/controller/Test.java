f9c049e260005ce0ae9a96f9c5d7ac19fe557edc
shuqingle <shuqingle_CN@163.com>
2021-11-08 20:54:48 +0800

    添加包目录

---
+++ sugo-resident-system/sugo-resident-manager/resident-console/src/main/java/com/sugo/resident/console/menus/controller/Test.java
@@ -0,0 +1,9 @@
+package com.sugo.resident.console.menus.controller;
+
+import com.sugo.resident.common.utils.CommonUtils;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(CommonUtils.getUUID());
+    }
+}
