9f8cbf5b629b82d56d6ec495706d9a637aab6600
shuqingle <shuqingle_CN@163.com>
2022-02-18 23:37:47 +0800

    代码简单调整

--- sugo-resident-system/sugo-resident-manager/resident-task/src/main/java/com/sugo/resident/task/ResidentTaskApplication.java
+++ sugo-resident-system/sugo-resident-manager/resident-task/src/main/java/com/sugo/resident/task/ResidentTaskApplication.java
@@ -3,7 +3,9 @@ package com.sugo.resident.task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//微服务必须扫描到commonApi的模块路径，否则无法获取对应的bean
+@SpringBootApplication(scanBasePackages = {"com.sugo.resident.task","com.sugo.resident.commonapi"})
+
 public class ResidentTaskApplication {
 
     public static void main(String[] args) {
