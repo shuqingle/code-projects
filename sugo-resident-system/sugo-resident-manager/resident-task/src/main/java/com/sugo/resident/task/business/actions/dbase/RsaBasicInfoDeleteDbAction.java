ae1966c08a59adc7d569411c9b7a322d5625aa23
shuqingle <shuqingle_CN@163.com>
2022-02-20 14:06:58 +0800

    处理注入异常的问题

--- sugo-resident-system/sugo-resident-manager/resident-task/src/main/java/com/sugo/resident/task/business/actions/dbase/RsaBasicInfoDeleteDbAction.java
+++ sugo-resident-system/sugo-resident-manager/resident-task/src/main/java/com/sugo/resident/task/business/actions/dbase/RsaBasicInfoDeleteDbAction.java
@@ -3,12 +3,13 @@ package com.sugo.resident.task.business.actions.dbase;
 import com.sugo.resident.commonapi.business.action.AbstractBusinessAction;
 import com.sugo.resident.commonapi.business.annot.BusiAutoWired;
 import com.sugo.resident.commonapi.business.base.BusinessBase;
+import com.sugo.resident.commonapi.business.utils.SpringContextUtil;
 import com.sugo.resident.task.dao.RRsaMapper;
 import com.sugo.resident.task.model.RsaDTO;
 
 public class RsaBasicInfoDeleteDbAction extends AbstractBusinessAction<RsaDTO> {
-    @BusiAutoWired
-    private RRsaMapper rsaMapper;
+//    @BusiAutoWired
+    private RRsaMapper rsaMapper = SpringContextUtil.getBean(RRsaMapper.class);
 
     public RsaBasicInfoDeleteDbAction(BusinessBase<RsaDTO> busi) throws Exception {
         super(busi);
--- sugo-resident-system/sugo-resident-manager/resident-task/src/main/java/com/sugo/resident/task/business/actions/dbase/RsaBasicInfoToDbAction.java
+++ sugo-resident-system/sugo-resident-manager/resident-task/src/main/java/com/sugo/resident/task/business/actions/dbase/RsaBasicInfoToDbAction.java
@@ -3,12 +3,13 @@ package com.sugo.resident.task.business.actions.dbase;
 import com.sugo.resident.commonapi.business.action.AbstractBusinessAction;
 import com.sugo.resident.commonapi.business.annot.BusiAutoWired;
 import com.sugo.resident.commonapi.business.base.BusinessBase;
+import com.sugo.resident.commonapi.business.utils.SpringContextUtil;
 import com.sugo.resident.task.dao.RRsaMapper;
 import com.sugo.resident.task.model.RsaDTO;
 
 public class RsaBasicInfoToDbAction extends AbstractBusinessAction<RsaDTO> {
     @BusiAutoWired
-    private RRsaMapper rsaMapper;
+    private RRsaMapper rsaMapper = SpringContextUtil.getBean(RRsaMapper.class);
     public RsaBasicInfoToDbAction(BusinessBase<RsaDTO> busi) throws Exception {
         super(busi);
     }
