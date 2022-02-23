98dabaa73fb8b36d6a07ece8a910e21cacc4d19a
shuqingle <shuqingle_CN@163.com>
2022-02-23 21:54:34 +0800

    新增获取公钥信息的接口

--- sugo-resident-system/sugo-resident-manager/resident-console/src/main/java/com/sugo/resident/console/security/service/impl/RSecureServiceImpl.java
+++ sugo-resident-system/sugo-resident-manager/resident-console/src/main/java/com/sugo/resident/console/security/service/impl/RSecureServiceImpl.java
@@ -1,4 +1,4 @@
-package com.sugo.resident.console.ruser.service.impl;
+package com.sugo.resident.console.security.service.impl;
 
 import com.sugo.resident.commonapi.business.process.AbstractBusinessProcess;
 import com.sugo.resident.console.ruser.business.process.UserLoginProcess;
@@ -6,6 +6,9 @@ import com.sugo.resident.console.ruser.dao.RUserMapper;
 import com.sugo.resident.console.ruser.model.UserInfoDto;
 import com.sugo.resident.console.ruser.model.UserInfoVO;
 import com.sugo.resident.console.ruser.service.RUserService;
+import com.sugo.resident.console.security.business.process.GetRsaPublicKeyProcess;
+import com.sugo.resident.console.security.model.RsaDTO;
+import com.sugo.resident.console.security.service.RSecureService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,13 +24,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 @Slf4j
-public class RuserServiceImpl implements RUserService {
+public class RSecureServiceImpl implements RSecureService {
 
     @Override
-    public UserInfoVO login(UserInfoDto userInfoDto) throws Exception {
-        //创建执行登录业务流程
-        AbstractBusinessProcess<UserInfoDto> process = new UserLoginProcess(userInfoDto);
-        //执行登录业务流程并返回用户信息
-		return process.executeWithResp().getUserInfoVO();
+    public String getPublicRsaKey(RsaDTO rsaDTO) throws Exception {
+        AbstractBusinessProcess<RsaDTO> process = new GetRsaPublicKeyProcess(rsaDTO);
+        return process.executeWithResp().getPublicKey();
     }
 }
